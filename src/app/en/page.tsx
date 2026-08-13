d171268d11f36060c3cb5ee0358f1a26bddf78c0
UnicoEnergyGroup <dain21co@gmail.com>
2026-08-14 00:51:22 +0900

    Add EnGrowthStrategy component

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -18,6 +18,7 @@ export default function EnHome() {
      <EnBusinessVideo />
      <EnCompetitiveness />
      <EnBusinessUnits />
+     <EnGrowthStrategy />
       </main>
       <EnFooter />
     </>
