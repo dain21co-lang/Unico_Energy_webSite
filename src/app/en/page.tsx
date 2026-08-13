cba5866830118ba5508b936b9df26164e3b660be
UnicoEnergyGroup <dain21co@gmail.com>
2026-08-13 19:39:48 +0900

    Add English version route structure

---
+++ src/app/en/page.tsx
@@ -0,0 +1,14 @@
+import { Nav } from "@/components/Nav";
+import { Footer } from "@/components/Footer";
+
+export default function EnHome() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <p style={{ color: "white", padding: "100px" }}>English version coming soon</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
