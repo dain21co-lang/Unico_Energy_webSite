a313bd72a8b73d68202bcdc329f9440201c572fc
UnicoEnergyGroup <dain21co@gmail.com>
2026-08-13 22:04:18 +0900

    Add EnAbout section to English page

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -1,13 +1,14 @@
 import { EnNav } from "@/components/EnNav";
 import { EnHero } from "@/components/EnHero";
 import { EnFooter } from "@/components/EnFooter";
-
+import { EnAbout } from "@/components/EnAbout";
 export default function EnHome() {
   return (
     <>
       <EnNav />
       <main>
         <EnHero />
+     <EnAbout />
       </main>
       <EnFooter />
     </>
